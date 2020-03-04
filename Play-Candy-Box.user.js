932d93be0b2554835c745f35cffda7e41475a658
Breno Ribeiro <brenodm@ciandt.com>
2020-03-04 11:06:30 -0300

    Add gitignore and URLs for automatic updates

--- Play-Candy-Box.user.js
+++ Play-Candy-Box.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Play Candy Box
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Automate some tasks from the first Candy Box
+// @namespace    http://ribeirobreno.com.br/
+// @version      0.2
+// @updateURL    https://github.com/ribeirobreno/candy-box-player/raw/master/Play-Candy-Box.meta.js
+// @downloadURL  https://github.com/ribeirobreno/candy-box-player/raw/master/Play-Candy-Box.user.js
+// @description  Automate some tasks from the first Candy Box game.
 // @author       Breno Ribeiro <ribeiro.breno@gmail.com>
 // @match        https://candybox2.github.io/candybox/
 // @grant        unsafeWindow
