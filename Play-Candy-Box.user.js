00b4a43a6f4c270becdc18a9bcbcb2809fec24d3
Breno Ribeiro <brenodm@ciandt.com>
2020-03-04 19:53:00 -0300

    Add buildable sources to centralize .meta.js content

--- Play-Candy-Box.user.js
+++ Play-Candy-Box.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Play Candy Box
 // @namespace    http://ribeirobreno.com.br/
-// @version      0.2
+// @version      0.3
 // @updateURL    https://github.com/ribeirobreno/candy-box-player/raw/master/Play-Candy-Box.meta.js
 // @downloadURL  https://github.com/ribeirobreno/candy-box-player/raw/master/Play-Candy-Box.user.js
 // @description  Automate some tasks from the first Candy Box game.
@@ -10,54 +10,4 @@
 // @grant        unsafeWindow
 // @run-at       document-idle
 // ==/UserScript==
-
-(function() {
-    'use strict';
-
-    var clicked = false,
-        el = document.createElement('div');
-    el.style = 'position:fixed;top:0;border:1px solid #080;background-color:#8F8;color:#030;padding:.125em .25em;text-align:center;font-family:SFMono-Regular,Consolas,"Liberation Mono",Menlo,monospace;';
-    el.innerHTML = 'HP soon!';
-    document.body.appendChild(el);
-
-    function draw() {
-        el.innerHTML = unsafeWindow.quest.getCharacterMaxHp();
-
-        requestAnimationFrame(draw);
-    }
-
-    function loop() {
-        var btn = document.getElementById('quest_button'),
-            dst = document.getElementById("quest_destination"),
-            eat = document.getElementById("eat");
-
-        if (btn && dst && dst.selectedIndex > -1 && !btn.disabled) {
-            if (clicked) {
-                clicked = false;
-                if (unsafeWindow.quest.things[unsafeWindow.quest.getCharacterIndex()].hp) {
-                    if (dst.options.length > (dst.selectedIndex + 1)) {
-                        ++dst.selectedIndex;
-                    }
-                } else if (dst.selectedIndex > 0) {
-                    --dst.selectedIndex;
-                    if (unsafeWindow.quest.getCharacterMaxHp() < (Number.MAX_SAFE_INTEGER/2) && eat) {
-                        eat.dispatchEvent(new Event('click'));
-                        el.innerHTML = unsafeWindow.quest.getCharacterMaxHp();
-                    }
-                }
-            }
-
-            btn.dispatchEvent(new Event('click'));
-            clicked = true;
-        }
-
-        if (dst && dst.selectedIndex < 0) {
-            dst.selectedIndex = 0;
-        }
-
-        setTimeout(loop, 1000/60);
-    }
-
-    setTimeout(loop, 1000/60);
-    requestAnimationFrame(draw);
-})();
+!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=0)}([function(e,t,n){e.exports=n(1)},function(e,t){!function(){"use strict";var e=!1,t=document.createElement("div");t.style='position:fixed;top:0;border:1px solid #080;background-color:#8F8;color:#030;padding:.125em .25em;text-align:center;font-family:SFMono-Regular,Consolas,"Liberation Mono",Menlo,monospace;',t.innerHTML="HP soon!",document.body.appendChild(t),setTimeout((function n(){var o=document.getElementById("quest_button"),r=document.getElementById("quest_destination"),i=document.getElementById("eat");o&&r&&r.selectedIndex>-1&&!o.disabled&&(e&&(e=!1,unsafeWindow.quest.things[unsafeWindow.quest.getCharacterIndex()].hp?r.options.length>r.selectedIndex+1&&++r.selectedIndex:r.selectedIndex>0&&(--r.selectedIndex,unsafeWindow.quest.getCharacterMaxHp()<Number.MAX_SAFE_INTEGER/2&&i&&(i.dispatchEvent(new Event("click")),t.innerHTML=unsafeWindow.quest.getCharacterMaxHp()))),o.dispatchEvent(new Event("click")),e=!0),r&&r.selectedIndex<0&&(r.selectedIndex=0),setTimeout(n,1e3/60)}),1e3/60),requestAnimationFrame((function e(){t.innerHTML=unsafeWindow.quest.getCharacterMaxHp(),requestAnimationFrame(e)}))}()}]);
---
+++ src/main.js
@@ -0,0 +1,50 @@
+(function() {
+    'use strict';
+
+    var clicked = false,
+        el = document.createElement('div');
+    el.style = 'position:fixed;top:0;border:1px solid #080;background-color:#8F8;color:#030;padding:.125em .25em;text-align:center;font-family:SFMono-Regular,Consolas,"Liberation Mono",Menlo,monospace;';
+    el.innerHTML = 'HP soon!';
+    document.body.appendChild(el);
+
+    function draw() {
+        el.innerHTML = unsafeWindow.quest.getCharacterMaxHp();
+
+        requestAnimationFrame(draw);
+    }
+
+    function loop() {
+        var btn = document.getElementById('quest_button'),
+            dst = document.getElementById("quest_destination"),
+            eat = document.getElementById("eat");
+
+        if (btn && dst && dst.selectedIndex > -1 && !btn.disabled) {
+            if (clicked) {
+                clicked = false;
+                if (unsafeWindow.quest.things[unsafeWindow.quest.getCharacterIndex()].hp) {
+                    if (dst.options.length > (dst.selectedIndex + 1)) {
+                        ++dst.selectedIndex;
+                    }
+                } else if (dst.selectedIndex > 0) {
+                    --dst.selectedIndex;
+                    if (unsafeWindow.quest.getCharacterMaxHp() < (Number.MAX_SAFE_INTEGER/2) && eat) {
+                        eat.dispatchEvent(new Event('click'));
+                        el.innerHTML = unsafeWindow.quest.getCharacterMaxHp();
+                    }
+                }
+            }
+
+            btn.dispatchEvent(new Event('click'));
+            clicked = true;
+        }
+
+        if (dst && dst.selectedIndex < 0) {
+            dst.selectedIndex = 0;
+        }
+
+        setTimeout(loop, 1000/60);
+    }
+
+    setTimeout(loop, 1000/60);
+    requestAnimationFrame(draw);
+})();
